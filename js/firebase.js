d68d8413400344b3d3e0fd41e779c6f41128274b
hokomili <28939939+hokomili@users.noreply.github.com>
2021-07-14 13:53:51 +0800

    forum

--- js/firebase.js
+++ js/firebase.js
@@ -10,7 +10,10 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var provider = new firebase.auth.GoogleAuthProvider();
 var db=firebase.firestore();
+var storage = firebase.storage();
+var storageRef=storage.refFromURL("gs://hackathon713.appspot.com/")
 var alldocRef = db.collection("allThreads");
+if(document.getElementById("google")){
 document.getElementById("google").onclick=()=>{
     firebase.auth()
     .signInWithPopup(provider)
@@ -33,9 +36,24 @@ document.getElementById("google").onclick=()=>{
         console.log(error)
     })
 }
+}
+var card=document.getElementById("card_template");
 alldocRef.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
+        let temp=document.createElement("div")
+        temp.id=doc.id
+        temp.innerHTML=card.innerHTML
+        temp.innerHTML=temp.innerHTML.replace('TITLE_HERE',doc.data().uid);
+        temp.innerHTML=temp.innerHTML.replace('INFO_HERE',doc.data().info);
+        storageRef.child(doc.data().imgName).getDownloadURL()
+        .then((url) => {
+            temp.innerHTML=temp.innerHTML.replace('URL_HERE',url);
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+        document.getElementsByClassName("section2")[0].appendChild(temp)
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, doc.data());
+        console.log(doc.id, JSON.stringify(doc.data().uid));
     });
 });
